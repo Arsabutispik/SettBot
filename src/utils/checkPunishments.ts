ad7aa4fbe2cf324a9a008bf7db0a1910a1e40582
Arsabutispik <gencoyucu765@gmail.com>
2022-04-15 14:47:52 +0300

    ğ

--- src/utils/checkPunishments.ts
+++ src/utils/checkPunishments.ts
@@ -1,5 +1,5 @@
 import { SettClient } from "../types";
-
+import modlog from "./modlog.js";
 import punishmentSchema from "../schemas/punishmentSchema.js";
 
 export default async(client: SettClient) => {
@@ -10,12 +10,14 @@ export default async(client: SettClient) => {
         const results = await punishmentSchema.find(query)
 
         for(const result of results) {
-            const {userId, type} = result
+            const {userId, type, staffId, createdAt, expires} = result
             const guild = await client.guilds.fetch("guild id")
+            const member = guild.members.cache.get(userId)
+            const staff = guild.members.cache.get(staffId)
             if(type == "ban") {
                 guild.members.unban(userId, "Ban süresi doldu")
+                modlog(guild, userId, "BAN_SÜRESİ", staff ? staff : staffId, "Ban süresi doldu", new Date(expires).getTime() - new Date(createdAt).getTime())
             } else if(type == "mute"){
-                const member = guild.members.cache.get(userId)
                 if(!member){
                     continue
                 }
