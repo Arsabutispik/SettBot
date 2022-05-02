b2ffcdc953565bd5977a9124ffc324684b9d396b
Arsabutispik <gencoyucu765@gmail.com>
2022-05-02 11:39:40 +0300

    fixlendi

--- src/utils/duelBoard.ts
+++ src/utils/duelBoard.ts
@@ -3,20 +3,18 @@ import userSchema from "../schemas/userSchema.js"
 import { TextChannel } from "discord.js"
 
 const fetchTopMembers = async() => {
-    let text = 'Sende arenaların kralı olmak istiyor musun? O zaman hemen düelloya başla!\n'
+    let text = 'Sende arenaların kralı olmak istiyor musun? O zaman hemen düelloya başla!\n\n'
     const results = await userSchema.find({}).sort({win: -1}).limit(10)
-    
-    for(let counter = 0; counter < 10; counter++) {
+    for(let counter = 0; counter < results.length; counter++) {
         const result = results[counter]
-
-        text += `#${counter + 1}. <@${result._id}> şu ana kadar **${result.win}** düello kazamış!\n`
+        text += `#${counter + 1}. <@${result._id}> şu ana kadar **${result.win}** düello kazamış!\n\n`
     }
     text += "Bu mesaj her 10 dakikada bir yenilenecektir."
     return text
 }
 
 const updateLeaderboard = async (client: SettClient) => {
-    const channel = client.channels.cache.get("kanal id") as TextChannel
+    const channel = client.channels.cache.get("970603107849478145") as TextChannel
     if(!channel) return
     const top = await fetchTopMembers()
     const repeatMessage = async() => {
@@ -25,7 +23,7 @@ const updateLeaderboard = async (client: SettClient) => {
         if(!sentMessage) {
             sentMessage = await channel.send(top)
         }
-        if(sentMessage.content === top) {
+        if(sentMessage.content.startsWith("Sende arenaların kralı olmak istiyor musun? O zaman hemen düelloya başla!")) {
             if(sentMessage.author.id === client.user?.id) {
                 sentMessage.edit(top)
             } else {
